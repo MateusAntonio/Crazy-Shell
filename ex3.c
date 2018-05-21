478c98bf6fc7fe78bf38bae31f8cced02e08cb59
luanripax <38923629+luanripax@users.noreply.github.com>
2018-05-21 16:02:32 -0300

    Add files via upload

--- ex3.c
+++ ex3.c
@@ -6,23 +6,32 @@
 #include <sys/wait.h>
 
 void crazy_exit(void);
+void crazy_wait(void);
 void prompt(void);
 void ctrl_c(int signo);
 void exec(char* exe, char* argv[]);
+void clear();
 
+int key=0;
 pid_t pidfilho; // global pq é necessária no handler ctrl_c
 
 int main(int argc, const char* argv[]){
     
-    while(1) { // loop infinito
+    pid_t son;
+    pid_t pai = getpid();
+    pid_t neto;
+    int aux;
+    
+    while(1) {
         
         char instrucao[100];  // linha toda que sera lida
-        char executavel[100]; // contera apenas o nome do executavel
-        char* argumentos[5];  // contera apenas os argumentos
+        char executavel[30]; // conterah apenas o nome do executavel
+        char* argumentos[5];  // conterah apenas os argumentos
         int i = 0;
         int j = 0;
         char* token;
         
+        //if(key == 0)
         prompt();
         
         scanf("%[^\n]", instrucao);
@@ -37,7 +46,7 @@ int main(int argc, const char* argv[]){
             i = 0;
             token = strtok(instrucao, " ");
             strcpy(executavel, token);
-         
+            
             
             while(j < ws){ // ao final, j sera o n de argumentos + comando
                 argumentos[i] = (char*)malloc((strlen(token)+1)*sizeof(char));
@@ -49,21 +58,36 @@ int main(int argc, const char* argv[]){
             //argumentos[i] = NULL;
             
             if(j < 6) { // exec
-                //execv(executavel, argumentos);
-                exec(executavel, argumentos);
-                //printf("comando: %s\n", executavel);
-                 //for(int x = 0; x <= j; x++){
-                   //  printf("%s\n", argumentos[x]);
-                 //}
+                
+                son = fork();
+                
+                if(getpid() == pai) {
+                    aux = waitpid(son, &aux, WUNTRACED);
+                }
+                
+                if(son == 0) {
+                    neto = fork();
+                    
+                    if (neto == 0) {
+                        daemon(0, 0);
+                    }
+                    
+                    execv(executavel, argumentos);
+                    
+                }
+                
             }
             else { // mais parâmetros do que o permitido
                 printf("Apenas sao permitidos ate 5 argumentos!\n");
             }
         } else if(strcmp(instrucao, "wait") == 0) { // wait
-            printf("comando wait detectado\n");
+            crazy_wait();
         } else if(strcmp(instrucao, "exit") == 0) { //exit
-            printf("adeus\n");
-            exit(0);
+            // liberar memoria
+            for (int x=0; x<j; x++) {
+                free(argumentos[x]);
+            }
+            crazy_exit();
         }
         else // comando incorreto
             printf("csh: Comando \"%s\" nao encontrado\n", instrucao);
@@ -77,6 +101,7 @@ void prompt(){
 
 void exec(char* exe, char* argv[]){
     
+    //signal(SIGINT, ctrl_c);
     int erro = 1;
     //printf("eii! %s %s\n", exe, argv[0]);
     //char *const ArgumentV[] = {"2015100338", "162534"}; // ver como isso funciona
@@ -92,28 +117,35 @@ void exec(char* exe, char* argv[]){
             fprintf(stderr, "Fork failed! \n");
             return ;
         } else if(pid2 == 0){ // grandson process
-            printf("print do neto\n");
-            daemon(0, 0);
-           // erro = execv(exe, argv);
+            //printf("print do neto\n");
+            //    daemon(0, 0);
+            erro = execv(exe, argv);
             if(erro == (-1))
                 printf("deu erro krlh");
-            //printf("Executou neto");
+            printf("Executou neto");
+            
         } else {
             pidfilho = pid;
-            //sleep(2);
-            printf("print do filho\n");
-            //erro = execv(exe, argv);
+            sleep(2);
+            //printf("print do filho\n");
+            erro = execv(exe, argv);
             if(erro == (-1))
                 printf("deu erro krlh");
-            //printf("Executou filho/pai");
+            printf("Executou filho/pai");
+            sleep(2);
+            
         }
-        erro = execv(exe, argv);
+        // erro = execv(exe, argv);
+        // if(erro == (-1))
+        //     printf("deu erro ai\n");
+        
         
     } else { //parent process
-        //signal(SIGINT, ctrl_c);
+        signal(SIGINT, ctrl_c);
         int status;
         waitpid(pidfilho, &status, WUNTRACED);
         printf("filho retornou! \n");
+        key=0;
     }
     //return
 }
@@ -125,8 +157,9 @@ void ctrl_c(int signo) {
     sigfillset(&sigset);
     sigprocmask(SIG_SETMASK, &sigset, &oldset);
     signal(signo, SIG_IGN); // ignora um possivel control c novo
-    printf("Não adianta me enviar um sinal por Ctrl-c, não vou morrer! Você quer"
-           "suspender meu filho que está rodando em foreground? S/n:");
+    prompt();
+    printf("Não adianta me enviar um sinal fpor Ctrl-c, não vou morrer! Você quer"
+           "suspender meu filho que está rodando em foreground? S/n:\n");
     c = getchar();
     if (c == 'S' || c == 's')
         kill(pidfilho, 20);
@@ -136,12 +169,37 @@ void ctrl_c(int signo) {
 }
 
 void crazy_exit() {
-    int aux;
+    pid_t pid;
     int status;
-    while(aux = waitpid(-1, &status, 0)) {
-        if(aux > 0)
-            printf("Esperanado filho liberar");
+    while(pid = waitpid(-1, &status, 0)) {
+        if(pid > 0)
+            printf("csh: Aguarde...\n");
         else
             break;
     }
+    exit(0);
 }
+
+void crazy_wait() {
+    
+    pid_t pid;
+    int status, filhos = 0;
+    while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        filhos++;
+        if(WIFSIGNALED(status))
+            printf("[INFO] O processo %d foi recebeu o sinal %d e foi finalizado", pid, WTERMSIG(status));
+        else if(WIFEXITED(status))
+            printf("[INFO]) O processo finalizou sem recebimento de sinais");
+    }
+    if(filhos)
+        printf("Filhos zumbis liberados");
+    else
+        printf("Sem filhos zumbi");
+}
+
+
+//void clear(){
+  //  if(!fork())
+    //    execlp("clear","clear",NULL);
+//}
+
